1d97a5097ad674d282c5b4e15e16da3d886137be
Tom Frumy <tom.frumy170@gmail.com>
2025-02-13 11:58:50 +0100

    projet v1.1

---
+++ src/lib/components/Projects/SmartDesk.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    export let expandedProject;
+    export let isExpanded = false;
+    const dispatch = createEventDispatcher();
+
+    function toggleExpand() {
+        dispatch('toggleExpand', 'SmartDesk');
+    }
+</script>
+
+<div class="project-container font-['Jersey25'] p-4 border border-[#FF4D00] rounded-lg bg-gray-800 text-white grid grid-cols-3 gap-4 items-start" on:click={toggleExpand} on:keydown={e => e.key === 'Enter' && toggleExpand()} role="button" tabindex="0" aria-expanded={isExpanded}>
+    <div class="project-details col-span-2">
+        <h2 class="project-title text-4xl">SmartDesk</h2>
+        <div class="project-divider h-[2px] bg-[#FF4D00] mb-4 w-full"></div>
+        <p class="project-description mb-4 text-2xl">
+            Ceci est une description du projet smart desk pour tester le rendu de la page.
+        </p>
+        <div class="project-tags flex flex-wrap gap-2">
+            <div class="tag text-white px-2 py-1 bg-cyan-800">#HTML</div>
+            <div class="tag text-white px-2 py-1 bg-cyan-800">#CSS</div>
+            <div class="tag text-white px-2 py-1 bg-cyan-800">#JS</div>
+        </div>
+    </div>
+    <div class="project-image flex items-center justify-center col-span-1 h-full relative">
+        <img src="/SmartDesk.png" alt="SmartDesk" class="responsive-image h-full">
+        <div class="plus-icon absolute flex items-center justify-center bg-[#FF4D00] text-white w-5 h-5 text-3xl right-0 top-0">
+            {isExpanded ? '-' : '+'}
+        </div>
+    </div>
+</div>
+
+<style>
+    .project-container {
+        width: 100%;
+        margin: 0 auto;
+        height: 100%;
+    }
+
+
+    .responsive-image {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .plus-icon {
+        z-index: 10;
+    }
+</style>
