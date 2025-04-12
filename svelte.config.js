4f125216c1d09ebfb39053130f08c46f4d97cf6b
Tom Frumy <146957280+Kurama73@users.noreply.github.com>
2025-04-12 21:47:35 +0200

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-export default {
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: '/PortFolio' // Chemin correspondant au nom du dépôt GitHub
-		}
-	},
-	preprocess: vitePreprocess()
+const config = {
+	kit: {}
 };
+
+export default config;
