06c5a93c7b9c599338bbcd6be8258c714ce29b52
Tom Frumy <146957280+Kurama73@users.noreply.github.com>
2025-04-13 13:49:31 +0200

    docs

--- docs/_app/immutable/entry/start.DILjMJHg.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/D5w2L4Ps.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: '',
+			base: '/docs',
 			relative: false
 		}
 	}
