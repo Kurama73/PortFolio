14207d8970d538189f24542ce6dc1fa02d92088b
Tom Frumy <146957280+Kurama73@users.noreply.github.com>
2025-04-13 14:07:02 +0200

    Fix chemins pour GitHub Pages

---
+++ build/_app/immutable/chunks/rf8tjXsI.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1l5m4ba)==null?void 0:s.base)??"/PortFolio";var a;const e=((a=globalThis.__sveltekit_1l5m4ba)==null?void 0:a.assets)??t;export{e as a,t as b};
