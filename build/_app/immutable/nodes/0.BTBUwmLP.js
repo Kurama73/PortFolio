8b024f3a806d3d3bb92388a73a5848effb67f767
Kurama73 <tom.frumy170@gmail.com>
2025-05-09 09:50:39 +0200

    fix button

---
+++ build/_app/immutable/nodes/0.BTBUwmLP.js
@@ -0,0 +1,9 @@
+import{c as f,h as l,a as s,t as i}from"../chunks/DG-db9dO.js";import{v as c,E as y,w as p,n as u,x as m,y as d,z as _,i as v}from"../chunks/Zo01nLfb.js";function h(o,n,...r){var a=o,e=u,t;c(()=>{e!==(e=n())&&(t&&(m(t),t=null),t=p(()=>e(a,...r)))},y),d&&(a=_)}const b=!0,g="always",T=Object.freeze(Object.defineProperty({__proto__:null,prerender:b,trailingSlash:g},Symbol.toStringTag,{value:"Module"}));var E=i(`<style>@font-face {
+          font-family: 'Jersey10';
+          src: url('/fonts/Jersey10.ttf') format('truetype');
+      }
+
+      @font-face {
+          font-family: 'Jersey25';
+          src: url('/fonts/Jersey25.ttf') format('truetype');
+      }</style>`);function j(o,n){var r=f();l(e=>{var t=E();s(e,t)});var a=v(r);h(a,()=>n.children),s(o,r)}export{j as component,T as universal};
